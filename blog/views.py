a9069f65e2137f471356662a3e64299ea937242f
Boyquotes <webmaster@linux-live-cd.org>
2017-06-23 12:10:03 +0200

    Update views.py
    Pas besoin d'avoir 2 fois from django.shortcuts import render
     dans le fichier
    Et ça
    from django.http import HttpResponse
    
    C'est mieux en haut de fichier comme ça quand en a 5 ou 6 c'est plus facile de savoir s'ils sont déjà importé ou pas...

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -19,8 +21,6 @@ def view_article(request, id_article):
 
 
 
-from django.http import HttpResponse
-from django.shortcuts import render
 def home(request):
     """ Exemple de page HTML, non valide pour que l'exemple soit concis """
     text = """<h1>Bienvenue sur mon blog !</h1>
